2480c3abb7307546336cc5bfb08c1a35316a42f4
FarhanHahmi968 <mdfarhan968@gmail.com>
2023-12-21 11:19:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// 1. You are provided with two numbers a and b. Your task is to add these two numbers.
+// let a= 2;
+// let b=5;
+// let c= a+b;
+// console.log(c)
+
+
+
+
+// 2. You are given two number first as a and second as b and check if both conditions
+// (a<10 and a>b) are satisfied or not with the help of operators.
+// let a=5;
+// let b=3;
+// console.log(a<10 && a>b)
+
+
+
+
+
+// 3 You are given two numbers a and b. You need to do the following checks:
+// 1. if both are divisible by 10 console true.
+// 2. if both are not divisible by 10 console false.
+// 3. if one of them only is divisible by 10 console true
+// let a = 10;
+// let b = 23;
+// console.log((a % 10) == 0 || (b % 10) == 0)
+
+
+
+// 4 Find the first digit of a 4 digit number
+// let a = 7625;
+// console.log(Math.floor(a / 1000))
+
+
+
+// 5. You are provided a four digit number a only. Your task is to print out the last digit of that
+// number.
+// let a = 4567;
+// console.log(a % 10)
+
+
+
+
+
+// 6 You are provided with two numbers a and b where a as divisor and b as dividend.Your task is find the remainder.
+// let a = 2;
+// let b = 9;
+// console.log(b % a)
+
+
+
+
+// 7 You are provided with two numbers a and b. Your task is to multiply these two numbers.
+// let a = 2;
+// let b = 5;
+// console.log(a * b)
+
+
+
+
+// 8 Shyam has got his marks in three subjects as a, b, and c(out of 100).Write a program to calculate his total marks and his average.
+// let a = 50;
+// let b = 20;
+// let c = 100;
+// let sum = a + b + c;
+// let average = (a + b + c) / 3;
+// console.log(sum, average)
